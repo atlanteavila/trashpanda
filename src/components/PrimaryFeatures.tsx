cc26f7e3d1eb8a0af3186ec13aa336f77f134591
Atlante Avila <atlanteavila@gmail.com>
2025-11-07 09:40:39 -0500

    update content on services

--- src/components/PrimaryFeatures.tsx
+++ src/components/PrimaryFeatures.tsx
@@ -76,11 +76,10 @@ export function PrimaryFeatures() {
       <Container className="relative">
         <div className="max-w-2xl md:mx-auto md:text-center xl:max-w-none">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-4xl md:text-5xl">
-            Everything you need to run your books.
+            Services that allow you to gain valued time back!
           </h2>
           <p className="mt-6 text-lg tracking-tight text-green-100">
-            Well everything you need if you aren’t that picky about minor
-            details like tax compliance.
+            Hire us to spare you with the mundain and pesky little tasks that can be affordably replaced!
           </p>
         </div>
         <TabGroup
