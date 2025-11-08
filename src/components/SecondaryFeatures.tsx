d3c3c82259c7695d9d3a9f2772327a2d4e746e7d
Atlante Avila <atlanteavila@gmail.com>
2025-11-08 14:36:14 -0500

    Added new images and service descriptions and features

--- src/components/SecondaryFeatures.tsx
+++ src/components/SecondaryFeatures.tsx
@@ -6,9 +6,15 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 import clsx from 'clsx'
 
 import { Container } from '@/components/Container'
-import screenshotContacts from '@/images/screenshots/contacts.png'
-import screenshotInventory from '@/images/screenshots/inventory.png'
-import screenshotProfitLoss from '@/images/screenshots/profit-loss.png'
+import familyFun from '@/images/screenshots/family-fun.png'
+import poopScoop from '@/images/screenshots/poop-scoop.png'
+import cleanBins from '@/images/screenshots/clean-bins.png'
+
+import {
+  ArrowsUpDownIcon,
+  GlobeAmericasIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline'
 
 interface Feature {
   name: React.ReactNode
@@ -21,34 +27,15 @@ interface Feature {
 const features: Array<Feature> = [
   {
     name: 'Simple',
-    summary: '🗑️ Trash Can Take-Out & Return',
+    summary: 'Trash Can Take-Out & Return',
     description:
       'Never miss trash day again — we roll your cans to the curb and bring them back once collected. Rain or shine, vacation or busy week, we make sure your cans are right where they should be, right on time.',
-    image: screenshotProfitLoss,
+    image: familyFun,
     icon: function ReportingIcon() {
       let id = useId()
       return (
         <>
-          <defs>
-            <linearGradient
-              id={id}
-              x1="11.5"
-              y1={18}
-              x2={36}
-              y2="15.5"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset=".194" stopColor="#fff" />
-              <stop offset={1} stopColor="#6692F1" />
-            </linearGradient>
-          </defs>
-          <path
-            d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
-            stroke={`url(#${id})`}
-            strokeWidth={2}
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
+          <TrashIcon className="text-white" />
         </>
       )
     },
@@ -58,29 +45,21 @@ const features: Array<Feature> = [
     summary: 'Bin Cleaning & Sanitizing',
     description: `Keep your bins fresh and odor-free with regular cleanings.
 We use eco-friendly, high-pressure washing to remove grime, bacteria, and that mystery smell no one wants to talk about.`,
-    image: screenshotInventory,
+    image: cleanBins,
     icon: function InventoryIcon() {
-      return <>✨</>
+      return <ArrowsUpDownIcon className='text-white' />
     },
   },
   {
     name: 'Pet awesome',
-    summary: '🐾 Yard Poop Scoop Service',
+    summary: ' Yard Poop Scoop Service',
     description: `Enjoy a clean, safe yard for kids and pets.
 We’ll remove pet waste, sanitize the area, and keep your outdoor space healthy and spotless — because no one likes unpleasant surprises on the lawn.`,
-    image: screenshotContacts,
+    image: poopScoop,
     icon: function ContactsIcon() {
       return (
         <>
-          <path
-            opacity=".5"
-            d="M25.778 25.778c.39.39 1.027.393 1.384-.028A11.952 11.952 0 0 0 30 18c0-6.627-5.373-12-12-12S6 11.373 6 18c0 2.954 1.067 5.659 2.838 7.75.357.421.993.419 1.384.028.39-.39.386-1.02.036-1.448A9.959 9.959 0 0 1 8 18c0-5.523 4.477-10 10-10s10 4.477 10 10a9.959 9.959 0 0 1-2.258 6.33c-.35.427-.354 1.058.036 1.448Z"
-            fill="#fff"
-          />
-          <path
-            d="M12 28.395V28a6 6 0 0 1 12 0v.395A11.945 11.945 0 0 1 18 30c-2.186 0-4.235-.584-6-1.605ZM21 16.5c0-1.933-.5-3.5-3-3.5s-3 1.567-3 3.5 1.343 3.5 3 3.5 3-1.567 3-3.5Z"
-            fill="#fff"
-          />
+          <GlobeAmericasIcon className="size-1 text-white" />
         </>
       )
     },
@@ -103,11 +82,12 @@ function Feature({
     >
       <div
         className={clsx(
-          'w-9 rounded-lg',
+          'flex items-center justify-center rounded-lg',
           isActive ? 'bg-green-600' : 'bg-slate-500',
+          'h-9 w-9', // container size
         )}
       >
-        <svg aria-hidden="true" className="h-9 w-9" fill="none">
+        <svg aria-hidden="true" className="h-6 w-6" fill="none">
           <feature.icon />
         </svg>
       </div>
