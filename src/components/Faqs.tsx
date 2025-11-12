e3a21d465c793b23382822c650d5727675bf4902
Atlante Avila <atlanteavila@gmail.com>
2025-11-11 21:33:25 -0500

    FAQs

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -8,47 +8,22 @@ const faqs = [
     {
       question: 'Is tax included?',
       answer:
-        'Well no, but if you move your company offshore you can probably ignore it.',
+        'All taxes should be calculated at check out.',
     },
     {
-      question: 'Can I pay for my subscription via purchase order?',
-      answer: 'Absolutely, we are happy to take your money in all forms.',
+      question: 'Can I pay by cash or check?',
+      answer: 'We only accept credit card payments via our website at the moment. Check back soon though. This might change!',
     },
   ],
   [
     {
-      question: 'What was that testimonial about tax fraud all about?',
+      question: 'Can I schedule a one-off service?',
       answer:
-        'The Trash Panda is just a software application, ultimately your books are your responsibility.',
-    },
-    {
-      question:
-        'The Trash Panda sounds horrible but why do I still feel compelled to purchase?',
-      answer:
-        'This is the power of excellent visual design. You just can’t resist it, no matter how poorly it actually functions.',
-    },
-    {
-      question:
-        'I found other companies called The Trash Panda, are you sure you can use this name?',
-      answer:
-        'Honestly not sure at all. We haven’t actually incorporated or anything, we just thought it sounded cool and made this website.',
+        'Yes! Please contact us to schedule your service today: 925-330-8798.',
     },
   ],
   [
-    {
-      question: 'How do you generate reports?',
-      answer:
-        'You just tell us what data you need a report for, and we get our kids to create beautiful charts for you using only the finest crayons.',
-    },
-    {
-      question: 'Can we expect more inventory features?',
-      answer: 'In life it’s really better to never expect anything at all.',
-    },
-    {
-      question: 'I lost my password, how do I get into my account?',
-      answer:
-        'Send us an email and we will send you a copy of our latest password spreadsheet so you can find your information.',
-    },
+   
   ],
 ]
 
@@ -76,8 +51,7 @@ export function Faqs() {
             Frequently asked questions
           </h2>
           <p className="mt-4 text-lg tracking-tight text-slate-700">
-            If you can’t find what you’re looking for, email our support team
-            and if you’re lucky someone will get back to you.
+            Here's some of the FAQs we've encountered so far. Let us know if you have any more questions!
           </p>
         </div>
         <ul
